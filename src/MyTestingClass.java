74ecdd21f93f7020c5734c9719588644acc2d612
almiinho <almazazamatov863@gmail.com>
2023-05-14 23:11:14 +0600

    commit

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,2 +1,21 @@
 public class MyTestingClass {
+    private int id;
+
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    @Override //  Return string representation of the object
+    public String toString() {
+        return "MyTestingClass{" +
+                "id=" + id +
+                '}';
+    }
+
+
 }
