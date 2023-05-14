21fab661e0661b8bbf108732e7ee4ee8d366ddf1
almiinho <almazazamatov863@gmail.com>
2023-05-14 22:21:14 +0600

    commit

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -11,6 +11,23 @@ public class MyHashTable<K, V> {
     public boolean isEmpty() {
         return size == 0;
     }
+    private static class Entry<K, V> {
+
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    public MyHashTable() {
+        table = new LinkedList[14]; // Creates a table with 14 linked lists
+        for (int i = 0; i < table.length; i++) {
+            table[i] = new LinkedList<Entry<K, V>>(); // Commence every linked list into array
+        }
+        size = 0;
+    }
 
 }
 
