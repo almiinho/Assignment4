22f76eae47b47f6db33f20ad7ea74b67075b948c
almiinho <almazazamatov863@gmail.com>
2023-05-14 23:18:53 +0600

    commit

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -12,9 +12,23 @@ public class MyTestingClass {
 
     @Override //  Return string representation of the object
     public String toString() {
-        return "MyTestingClass{" +
-                "id=" + id +
-                '}';
+        return "MyTestingClass{" + "id=" + id + "}";
+    }
+
+    @Override //  Generate unique hash codes for objects
+    public int hashCode() {
+        int res = 14;
+        res = 31 * res + id;
+        return res;
+    }
+
+
+    @Override// Compare objects  based on their id
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) o;
+        return id == that.id;
     }
 
 
