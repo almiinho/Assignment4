95ae427bd5c13e295c8a2058875a85f0934c84ef
almiinho <almazazamatov863@gmail.com>
2023-05-14 23:22:54 +0600

    commit

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,28 @@
 public class Student {
     private String name;
     private int id;
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }// Getter method for the name field
+
+
+    public void setName(String name) {
+        this.name = name;
+    } // Setter method for the name field
+
+
+    public int getId() {
+        return id;
+    }// Getter method for the id field
+
+
+    public void setId(int id) {
+        this.id = id;
+    }// Setter method for the id field
 }
